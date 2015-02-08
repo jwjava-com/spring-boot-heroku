7ad11869afd74cff85528e5ba4a4d51e3edef33f
Rob Mills <rmills@peapod.com>
2015-02-08 17:26:43 -0600

    Basic Spring Boot/Heroku application

---
+++ src/main/java/com/rgm/app/Application.java
@@ -0,0 +1,16 @@
+package com.rgm.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * @author rmills
+ */
+@EnableAutoConfiguration
+@ComponentScan("com.rgm.app")
+public class Application {
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(Application.class, args);
+	}
+}
