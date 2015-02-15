1283d13f780787d65567e435d1406a01ddc9610e
Rob Mills <rmills@peapod.com>
2015-02-15 16:27:00 -0600

    Add API path and update security config for access

--- src/main/java/com/rgm/app/Application.java
+++ src/main/java/com/rgm/app/Application.java
@@ -2,6 +2,7 @@ package com.rgm.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
@@ -9,6 +10,7 @@ import org.springframework.context.annotation.ComponentScan;
  */
 @EnableAutoConfiguration
 @ComponentScan("com.rgm.app")
+@SpringBootApplication
 public class Application {
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(Application.class, args);
--- src/main/java/com/rgm/app/WebSecurityConfig.java
+++ src/main/java/com/rgm/app/WebSecurityConfig.java
@@ -14,7 +14,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 			.authorizeRequests()
-				.antMatchers("/").permitAll()
+				.antMatchers("/","/api","/api/*").permitAll()
 				.anyRequest().authenticated()
 				.and()
 			.formLogin()
